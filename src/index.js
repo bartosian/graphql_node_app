d8f726a9c0ddd23f1d25497bd4ee0248db18485b
Kiryl Bartashevich <kbartashevich@administrators-MacBook-Pro.local>
2018-10-31 17:31:52 -0400

    Added mutation type

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+
+
+
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+    id: ID!
+    description: String!
+    url: String!
 }
 `
-
-console.log('Hi');
-
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews Clone`
+        info: () => `This is the API of a Hackernews Clone`,
+        feed: () => links
+    },
+    Link: {
+        id: (root) => root.id,
+        description: (root) => root.description,
+        url: (root) => root.url
     }
 }
 
