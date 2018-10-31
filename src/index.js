2c4135a87ed3ad9c61735ce80d0f2dabdadb8013
Kiryl Bartashevich <kbartashevich@administrators-MacBook-Pro.local>
2018-10-31 17:22:07 -0400

    Created new type

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+type Query {
+    info: String!
+}
+`
+
+console.log('Hi');
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews Clone`
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => console.log(`Server is running on 4000`))
