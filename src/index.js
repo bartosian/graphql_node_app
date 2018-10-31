8f38d30f9fd1ae4e38faf585a426223fb7c6762b
Kiryl Bartashevich <kbartashevich@administrators-MacBook-Pro.local>
2018-10-31 17:49:51 -0400

    Extended feature mutation

--- src/index.js
+++ src/index.js
@@ -6,20 +6,8 @@ let links = [{
     description: 'Fullstack tutorial for GraphQL'
   }]
 
+  let idCount = links.length;
 
-
-const typeDefs = `
-type Query {
-    info: String!
-    feed: [Link!]!
-}
-
-type Link {
-    id: ID!
-    description: String!
-    url: String!
-}
-`
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
@@ -29,12 +17,37 @@ const resolvers = {
         id: (root) => root.id,
         description: (root) => root.description,
         url: (root) => root.url
-    }
+    },
+    Mutation: {
+        post: (root, args) => {
+           const link = {
+            id: `link-${idCount++}`,
+            description: args.description,
+            url: args.url,
+          }
+          links.push(link)
+          return link
+        }
+      }
 }
 
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+    typeDefs: `
+    type Query {
+        info: String!
+        feed: [Link!]!
+    }
+    
+    type Mutation {
+        post(url: String!, description: String!): Link!
+    }
+    
+    type Link {
+        id: ID!
+        description: String!
+        url: String!
+    }`,
+    resolvers,
+  })
 
 server.start(() => console.log(`Server is running on 4000`))
